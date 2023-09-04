ec00cd9ec144dd5c77de3b5f7c087f93fd17322a
unknown <24100303@lums.edu.pk>
2023-09-04 13:22:35 +0500

    edit2

---
+++ src/components/other/other.js
@@ -0,0 +1,210 @@
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+
+export default function Other() {
+  return (
+  <Container fluid className="about-section">
+         <Container>
+        <Row style={{ justifyContent: "center", padding: "10px" }}>
+          <h1 style={{ fontSize: "2.4em", paddingBottom: "20px" }}>
+            Extra<strong className="teal">Curricular</strong>
+          </h1>
+          <ul style={{ listStyle: "none", padding: 0 }}>
+            <li style={{ fontSize: "1.2em" }}>
+            Batch Representative 
+               <strong>
+                <a
+                  href=""
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="teal-link"
+                >
+                , LUMS Student Council
+                </a>
+              </strong>
+            </li>
+            <li style={{ fontSize: "1em" }}>
+            I got elected as a batch Representative at Student Council of LUMS, which represented a student body of more than 3000. I secured 155/216 votes to win my seat. For the year 2022-2023, I was part of this council. 
+            </li>
+          </ul>
+
+          <ul style={{ listStyle: "none", padding: 0 }}>
+            <li style={{ fontSize: "1.2em" }}>
+              Chair   
+              <strong>
+                <a
+                  href=""
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="teal-link"
+                >
+                , Welfare Committee
+                </a>
+              </strong>
+              , 2022-2023
+            </li>
+            <li style={{ fontSize: "1em" }}>
+              I was chair for Student's council welfare committee. Our committee aimed at helping the needy amongst the LUMS community and collected funds for more than 100 deserving cases. In total, we raised over <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="teal-link"
+                >Pkr 2 million</a> which were distributed to deserving individuals. 
+            </li>
+          </ul>
+
+          <ul style={{ listStyle: "none", padding: 0 }}>
+            <li style={{ fontSize: "1.2em" }}>
+            Chair
+              <strong>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="teal-link"
+                >
+                , Academic Committee 2022-2023
+                </a>
+              </strong>
+            </li>
+            <li style={{ fontSize: "1em" }}>
+              Add text here
+            </li>
+          </ul>
+
+          <ul style={{ listStyle: "none", padding: 0 }}>
+            <li style={{ fontSize: "1.2em" }}>
+              Director
+              <strong>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="teal-link"
+                >
+                , PhotoLUMS
+                </a>
+              </strong>
+            </li>
+            <li style={{ fontSize: "1em" }}>
+             I joined LUMS Photographic Scoiety in year 2021. Initially I was a team member of Logistics department and after an year of work there, I was promoted the next year to lead the department. My experience at this student society has been a memorable one as I learned and honed my skills to organise and manage events like LAPS, which the biggest photography competition in Pakistan. Through my time here, I learned mportant skills like Leadership, and teamwork.  
+            </li>
+          </ul>
+
+          <ul style={{ listStyle: "none", padding: 0 }}>
+            <li style={{ fontSize: "1.2em" }}>
+              <strong>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="teal-link"
+                >
+                Debates and MUNs
+                </a>
+              </strong>
+            </li>
+            <li style={{ fontSize: "1em" }}>
+              Add text here
+            </li>
+          </ul>
+
+          <ul style={{ listStyle: "none", padding: 0 }}>
+            <li style={{ fontSize: "1.2em" }}>
+            Assitant Director
+              <strong>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="teal-link"
+                >
+                , LUMS Student Mathematics Scoiety 
+                </a>
+              </strong>
+            </li>
+            <li style={{ fontSize: "1em" }}>
+              Add text here
+            </li>
+          </ul>
+
+        </Row>
+
+        <Row style={{ justifyContent: "center", padding: "10px" }}>
+          <h1 style={{ fontSize: "2.4em", paddingBottom: "20px" }}>
+            Co<strong className="teal">Curricular</strong>
+          </h1>
+
+          <ul style={{ listStyle: "none", padding: 0 }}>
+            <li style={{ fontSize: "1.2em" }}>
+            Teaching Assitant 
+              <strong>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="teal-link"
+                >
+                ,  Computational Problem Solving (CS100) Summer 2022
+                </a>
+              </strong>
+            </li>
+            <li style={{ fontSize: "1em" }}>
+              Add text here
+            </li>
+          </ul>
+
+          <ul style={{ listStyle: "none", padding: 0 }}>
+            <li style={{ fontSize: "1.2em" }}>
+            Head Teaching Assitant 
+              <strong>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="teal-link"
+                >
+                ,  Computational Problem Solving (CS100) Fall 2022-2023
+                </a>
+              </strong>
+            </li>
+            <li style={{ fontSize: "1em" }}>
+              Led a team of 8 Teaching Assitants to work together and provide assistance to student and oversee the programming labs. This is the Computer related course that freshmen take my team helped in making their experiences as fun and learning intensive as they could. The class had a size of over 90 students and I am proud of my team's and my effort in organising that course in best of manner. 
+            </li>
+          </ul>
+
+          <ul style={{ listStyle: "none", padding: 0 }}>
+            <li style={{ fontSize: "1.2em" }}>
+            DAAD Exchange Program 
+              <strong>
+                <a
+                  href="https://www.uni-saarland.de/en/home.html"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="teal-link"
+                >
+                , Universität des Saarlandes 
+                </a>
+              </strong>
+            </li>
+            <li style={{ fontSize: "1em" }}>
+              Add text here
+            </li>
+          </ul>
+
+
+        </Row>
+        {/* <Row style={{ justifyContent: "center", padding: "10px" }}>
+            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+              Art & <strong className="teal">Calligraphy</strong>
+            </h1>
+            <p style={{fontSize: "1.2em"}}>
+                My art account:  
+                <strong>
+                <a
+                  href="https://www.instagram.com/artbysaleha"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="teal-link"
+                > @artbysaleha
+                </a>
+              </strong>
+            </p> */}
+  {/* <PhotoAlbum layout="rows" photos={photos} /> */}
+  {/* </Row> */}
+  </Container>
+  </Container> );
+}
